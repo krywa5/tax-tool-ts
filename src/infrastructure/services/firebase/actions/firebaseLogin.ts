afe7b8a40a063c39e0ba56c4db4dd242b94743d8
krywa5 <blocker92@o2.pl>
2023-03-06 20:26:50 +0100

    Added github pages, cleaned errors from console

--- src/infrastructure/services/firebase/actions/firebaseLogin.ts
+++ src/infrastructure/services/firebase/actions/firebaseLogin.ts
@@ -1,6 +1,9 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, UserCredential } from "firebase/auth";
 import { firebaseAuth } from "infrastructure/services/firebase/firebase.service";
 
-export const firebaseLogin = async (email: string, password: string) => {
+export const firebaseLogin = async (
+  email: string,
+  password: string,
+): Promise<UserCredential> => {
   return await signInWithEmailAndPassword(firebaseAuth, email, password);
 };
